c9d8966c74dc27e65032deae74b8dd7064781620
Millie <Millie@MacBook-Pro-Kamilla.local>
2022-07-29 00:41:19 +0300

    adding info in postgre db

---
+++ postgre_start/go.mod
@@ -0,0 +1,5 @@
+module postgre_start
+
+go 1.18
+
+require github.com/lib/pq v1.10.6 // indirect
---
+++ postgre_start/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "heykamidemo"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `
+INSERT INTO users (age, email, first_name, last_name)
+VALUES ($1, $2, $3, $4)
+RETURNING id`
+	id := 0
+	err = db.QueryRow(sqlStatement, 25, "nbenjami@student.21-school.ru", "Nancee", "Benjamin").Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("New record ID is:", id)
+}
